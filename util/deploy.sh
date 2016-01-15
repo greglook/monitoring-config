9ad37855a7106d99906c318dc0ae35ff5516d0d1
Greg Look <greg@greg-look.net>
2016-01-14 20:02:03 -0800

    Update deployment script.

--- util/deploy.sh
+++ util/deploy.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 # Script to deploy the riemann rules to the server and reload the streams.
 
-RIEMANN_LIB_DIR=/etc/riemann/lib
-RIEMANN_CFG_DIR=/etc/riemann/config
+RIEMANN_CFG_DIR=/usr/local/etc/riemann
+RIEMANN_LIB_DIR=$RIEMANN_CFG_DIR/lib
 
 RSYNC_CMD=$(which rsync)
 RSYNC_OPTS="--verbose --recursive --delete"
 
-if [[ -n "$RSYNC_CMD" ]]; then
+if [[ -z "$RSYNC_CMD" ]]; then
     echo "Unable to locate rsync command!" >&2
     exit 1
 fi
@@ -17,6 +17,6 @@ RIEMANN_HOST=${1:-riemann}
 
 echo "Deploying riemann stream configuration to $RIEMANN_HOST"
 
-$RSYNC_CMD $RSYNC_OPTS config.clj $RIEMANN_HOST:$RIEMANN_CFG_DIR/config.clj
-$RSYNC_CMD $RSYNC_OPTS src/       $RIEMANN_HOST:$RIEMANN_LIB_DIR
-ssh $RIEMANN_HOST "service riemann status && sudo service riemann reload || sudo service riemann start"
+scp config.clj $RIEMANN_HOST:$RIEMANN_CFG_DIR/riemann.config
+$RSYNC_CMD $RSYNC_OPTS src/ $RIEMANN_HOST:$RIEMANN_LIB_DIR
+ssh $RIEMANN_HOST "service riemann status && sudo kill -s HUP \`sudo cat /var/run/riemann/riemann.pid\` || sudo service riemann start"
