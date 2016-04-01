6ba62f9b66be85c71f737d6175f33f6be66cc08c
Greg Look <greg@greg-look.net>
2016-04-01 11:17:37 -0700

    Remove leiningen project def.

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject greglook/riemann-config "0.1.0-SNAPSHOT"
-  :description "Riemann stream configuration rules."
-  :url "https://github.com/greglook/riemann-config"
-
-  :aliases {"test-config" ["run" "-m" "riemann.bin" "test" "config.clj"]}
-
-  :dependencies
-  [[org.clojure/clojure "1.7.0"]
-   [riemann "0.2.10"]])
